abb0e1904cc2e669457bb97b70478b24b07eb1d3
JNRCHAYAN <jogendranathroy40@gmail.com>
2026-06-07 12:07:40 +0600

    Update prediction api

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+import pickle
+import numpy as np
+import pandas as pd
+
+
 
 app = FastAPI()
+
+# Load Model
+model = pickle.load(open('regmodel.pkl','rb'))
+scalar = pickle.load(open('scaling.pkl','rb'))
 
 templates = Jinja2Templates(directory="templates")
 
@@ -20,3 +29,15 @@ async def home(request: Request):
         name="about.html",
         context={}
     )
+
+@app.post("/predict")
+async def predict(request: Request):
+    data = await request.json()
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output = model.predict(new_data)
+    print(output[0])
+    return {"prediction": float(output[0])}
+
+
